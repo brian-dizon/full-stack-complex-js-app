5b30cdd196f8ee352565df0b890b3a17ca6ee136
brian-dizon <140157607+brian-dizon@users.noreply.github.com>
2025-12-06 09:46:31 +0800

    Lesson 51 - model validation complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,9 +9,14 @@ exports.logout = (req,res)=> {
 }
 
 exports.register = (req,res)=> {
-    let user = new User();
+    let user = new User(req.body);
     user.register();
-    res.send('You tried to register.');
+    
+    if(user.errors.length){
+        res.send(user.errors);
+    } else {
+        res.send('Success. No errors found.')
+    }
 }
 
 exports.home = (req,res)=> {
