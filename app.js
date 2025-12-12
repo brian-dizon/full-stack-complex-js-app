ecea10aa666db218631a57998ab34dce09eb1db4
brian-dizon <140157607+brian-dizon@users.noreply.github.com>
2025-12-12 21:25:30 +0800

    Lessons 68 and 69 - user registration improvements

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
-const express = require('express');
-const session = require('express-session');
-const MongoStore = require('connect-mongo').default;
-const flash = require('connect-flash');
+const express = require("express");
+const session = require("express-session");
+const MongoStore = require("connect-mongo").default;
+const flash = require("connect-flash");
 const app = express();
 
-
 let sessionOptions = session({
-    secret: 'JS is awesome',
-    store: new MongoStore({ client: require('./db'), dbName: 'ComplexApp' }),
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24,
-        httpOnly: true,
-        sameSite: "strict"
-    }
+  secret: "JS is awesome",
+  store: new MongoStore({ client: require("./db"), dbName: "ComplexApp" }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+    sameSite: "strict",
+  },
 });
 
 app.use(sessionOptions);
 app.use(flash());
 
-const router = require('./router');
+const router = require("./router");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static('public'));
-app.set('views', 'views');
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("views", "views");
+app.set("view engine", "ejs");
 
-app.use('/', router)
+app.use("/", router);
 
 // app.listen(3000);
 module.exports = app;
